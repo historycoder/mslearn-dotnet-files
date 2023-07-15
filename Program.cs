15ef199e73a8f57069dd1397c9d51851ee789e94
Lauren Mitchell <sortsearch@gmail.com>
2023-07-15 19:05:07 -0400

    Unit 3 Exercise - Work with the file system

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+var salesFiles = FindFiles("stores");
+
+foreach (var salesFile in salesFiles){
+    Console.WriteLine(salesFile);
+}
+
+static IEnumerable<string> FindFiles(string folderName) {
+    List<string> salesFiles = new();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var foundFile in foundFiles) {
+        if (foundFile.EndsWith("sales.json")){
+            salesFiles.Add(foundFile);
+        }
+    }
+
+    return salesFiles;
+}
