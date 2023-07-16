646fcab079dbdf1d21a570062557a197391bd695
Lauren Mitchell <sortsearch@gmail.com>
2023-07-15 20:20:26 -0400

    Unit 9 Exercise - Read and write to files

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
@@ -8,8 +7,9 @@ var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
 Directory.CreateDirectory(salesTotalDir);
 
 var salesFiles = FindFiles(storesDirectory);
+var salesTotal = CalculateSalesTotal(salesFiles);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 static IEnumerable<string> FindFiles(string folderName) {
     List<string> salesFiles = new();
@@ -18,10 +18,29 @@ static IEnumerable<string> FindFiles(string folderName) {
 
     foreach (var foundFile in foundFiles) {
         var extension = Path.GetExtension(foundFile);
-        if (extension == ".json"){
+        if (extension == ".json") {
             salesFiles.Add(foundFile);
         }
     }
 
     return salesFiles;
 }
+
+static double CalculateSalesTotal(IEnumerable<string> salesFiles) {
+    double salesTotal = 0;
+
+    //Loop over each file path in salesFiles
+    foreach (var salesFile in salesFiles){
+        //Read the contents of the file
+        string salesJson = File.ReadAllText(salesFile);
+
+        //Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        //Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+record SalesData(double Total);
